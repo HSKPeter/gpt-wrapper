9f2539e6a9bb30058b650889b2d41c551633f996
Peter Hui <huishikuen@gmail.com>
2024-01-31 01:44:41 -0800

    Add response example for example2

--- src/example.ts
+++ src/example.ts
@@ -3,8 +3,8 @@ import { CodeExplainer, MathTutor } from "./agent";
 export async function example1() {
     const codeExplainer = new CodeExplainer();
     const response = await codeExplainer.explainCode("def foo():\n    print('Hello, world!')\n\nfoo()");
+    
     console.log(response);
-
     // Example of response:
     // This code defines a function called `foo` that prints the message "Hello, world!". It then calls the `foo` function, causing the message to be printed.
 }
@@ -12,5 +12,8 @@ export async function example1() {
 export async function example2() { 
     const mathTutor = new MathTutor();
     const response = await mathTutor.solveMathEquation("2 + 2");
+    
     console.log(response);
+    // Example of response:
+    // The solution to the equation \(2 + 2\) is \(4\).
 }
