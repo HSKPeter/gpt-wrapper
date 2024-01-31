e9fd2a6fbf537278bc94e8e9b27b8bc17221b06a
Peter Hui <huishikuen@gmail.com>
2024-01-31 01:02:13 -0800

    Fix bugs in MathTutor

--- src/agent/betaAssistant/mathTutor.ts
+++ src/agent/betaAssistant/mathTutor.ts
@@ -1,7 +1,7 @@
 import OpenAiBetaAssistant from "./base";
 
 export default class MathTutor extends OpenAiBetaAssistant {
-    protected static assistantName: string = "Math Tutor";
+    protected assistantName: string = "Math Tutor";
 
-    protected static instructions: string = "You are a personal math tutor. Write and run code to answer math questions.";
+    protected instructions: string = "You are a personal math tutor. Write and run code to answer math questions.";
 }
